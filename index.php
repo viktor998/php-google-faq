c3574338a5ad363ea986990d1357f896ea0a968b
Viktor Casubolo <viktor.casubolo98@gmail.com>
2021-05-01 15:01:57 +0200

    header terminato

---
+++ index.php
@@ -0,0 +1,47 @@
+<?php 
+    include 'dati.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./assets/css/master.css">
+</head>
+<body>
+
+    <header class='mr-5 ml-5'>
+        <div class="up pt-5 pb-4">
+            <div class="logo">
+                <img src="https://www.gstatic.com/images/branding/googlelogo/svg/googlelogo_clr_74x24px.svg" alt="">
+                <span class="ml-2">Privacy e termini</span>
+            </div>
+            <div class="right-side">
+                <svg class="gb_We mr-4" focusable="false" viewBox="0 0 24 24">
+                    <path fill= '#5f6368' d="M6,8c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM12,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM6,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM6,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM12,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM16,6c0,1.1 0.9,2 2,2s2,-0.9 2,-2 -0.9,-2 -2,-2 -2,0.9 -2,2zM12,8c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM18,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM18,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2z"></path>
+                </svg>
+                <div class="account-pic">
+                    <h3>V</h3>
+                </div>
+            </div>
+        </div>
+
+        <div class="down">
+            <ul>
+                <li><a href="#">Introduzione</a></li>
+                <li><a href="#">Norme sulla privacy</a></li>
+                <li><a href="#">Termini di servizio</a></li>
+                <li><a href="#">Tecnologie</a></li>
+                <li class='active'><a href="#">Domande frequenti</a></li>
+            </ul>
+        </div>
+        
+    </header>
+    <hr>
+</body>
+</html>
+
+<!-- url('https://www.gstatic.com/images/branding/googlelogo/svg/googlelogo_clr_74x24px.svg') -->
